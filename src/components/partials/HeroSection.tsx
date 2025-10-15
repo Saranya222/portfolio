5c611c9e2b7ef243033c9204950e8557649077b2
saranya tadikonda <111saranya111@berkeley.edu>
2025-10-15 15:33:22 -0500

    added headshot

--- src/components/partials/HeroSection.tsx
+++ src/components/partials/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       <HeroBackground />
       <div className="flex flex-col items-center">
         <div className="overflow-hidden rounded-full">
-          <Image src="/images/avatar/headshot.png" width={225} height={225} alt="avatar" />
+          <Image src="/images/avatar/Headshot.jpeg" width={225} height={225} alt="avatar" />
         </div>
         <h1 className="mt-4 text-3xl font-bold ">Saranya Tadikonda</h1>
         <div className="mt-2 flex gap-1">
